f2a7cffa3547a061d086eb65d205cf1bf192b769
R0lton <108031948+R0lton@users.noreply.github.com>
2024-08-16 22:08:02 +0300

    coomit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
+import logo from './img/check-logo.png';
+import apple from './img/avatars/apple.png';
+import pizza from './img/avatars/pizza.png'; 
+import burger from './img/avatars/burger.png'; 
+import hotdog from './img/avatars/hotdog.png'; 
+import sushi from './img/avatars/sushi.png'; 
+import taco from './img/avatars/taco.png'; 
+import frenchFries from './img/avatars/french-fries.png'; 
+import iceCream from './img/avatars/ice-cream.png'; 
+import banana from './img/avatars/banana.png'; 
+import donut from './img/avatars/donut.png'; 
+import pancakes from './img/avatars/pancakes.png'; 
+import coffee from './img/avatars/coffee.png'; 
 import './App.css';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
 
 function App() {
+  const location = useLocation();
+  const { value } = location.state || { value: 0 }; 
+  const [showModal, setShowModal] = useState(false);
+
+  const avatars = [
+    pizza,
+    burger,
+    hotdog,
+    sushi,
+    taco,
+    frenchFries,
+    apple,
+    banana,
+    donut,
+    pancakes,
+    coffee,
+    iceCream
+  ]
+
+  function roundTo(num, precision) {
+    const factor = Math.pow(10, precision)
+    return Math.round(num * factor) / factor
+  }
+
+  const items = Array.from({ length: value }, (_, index) => (
+    
+    <li key={index} className='person-info'>
+      <img src={avatars[index]} alt='P' className='person-img' />
+      <div className='person'>
+        <span className={`cost-${index} cost-per-person`}>0</span>
+      </div>
+    </li>
+  ));
+
+  const addCost = (event) => {
+    let idx = event.target.alt;
+    let cost = document.querySelector(`.cost-${idx}`);
+    let currSum = document.querySelector('.curr-sum');
+    
+    if (currSum.value !== '') {    
+      cost.textContent = (parseInt(cost.textContent) + parseInt(currSum.value)).toString();
+    }
+    currSum.value = '';
+    setShowModal(false);
+  };
+
+  const splitCost = () => {
+    let currSum = document.querySelector('.curr-sum');
+    
+    if (currSum.value !== '') {  
+      for (let idx = 0; idx < value; idx++) {
+        document.querySelector(`.cost-${idx}`).textContent = roundTo(parseInt(document.querySelector(`.cost-${idx}`).textContent) + (parseInt(currSum.value) / value), 2).toString();
+      }
+    }
+    currSum.value = '';
+
+  };
+
+  const choosing = Array.from({ length: Math.min(value, 12) }, (_, index) => (
+    <div key={index} className="avatar">
+      <img src={avatars[index]} alt={index} onClick={addCost} />
+    </div>
+  ));
+
+  const handleChooseClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseClick = () => {
+    setShowModal(false);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Link to='/' className='back-btn'>Reset</Link>
+      <header>
+      <img src={logo} alt='logo' className='logo-img-app' />
+      <span><span className='text'>check, please!</span></span>
       </header>
+      <div className='all-pay-sum'>
+        <input type='number' placeholder='0' className='curr-sum' />
+        <button className='choose' onClick={handleChooseClick}>choose</button>
+        <button className='for-all' onClick={splitCost}>split</button>
+      </div>
+      <div className='main-container'>
+          <ul>
+            {items}
+          </ul>
+      </div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <button className="close-button" onClick={handleCloseClick}>
+              &times;
+            </button>
+            <div className="avatar-grid">
+              {choosing}
+            </div>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,64 @@
+import logo from './img/check-logo.png';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Home.css';
+
+
+
+function Home() {
+    const [count, setCount] = useState(0);
+    const navigate = useNavigate();
+
+    const morePeople = () => {
+        if (count < 12) {
+            setCount(count + 1);
+        } else {
+            setCount(12);
+        }
+    };
+    const lessPeople = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        } else {
+            setCount(0);
+        }
+    };
+
+    const handleChange = (event) => {
+        let val = event.target.value;
+        if (!isNaN(val) && val !== '') {
+            
+            if (parseInt(val) > 12) {
+                setCount(12);
+            } else if (parseInt(val) < 0) {
+                setCount(0);
+            } else {
+                setCount(Number(val));
+            }
+        } else {
+            setCount(0); // Или можно игнорировать ввод, не изменяя count
+        }
+    };
+
+    const handleSubmit = () => {
+        navigate('/app', { state: { value: count } });
+    };
+    
+    return (
+        <div className="Home">
+            <img src={logo} alt='logo' className='logo-img' />
+            <div className='main-content'>
+                <div className='enter-menu'>
+                    <input type='button' className='less' value='-' onTouchEnd={lessPeople} />
+                    <input className='count' value={count} onChange={handleChange} />
+                    <input type='button' className='more' value='+' onTouchEnd={morePeople} /> 
+                </div>
+                <button className='success-btn-enter' 
+                    onTouchEnd={handleSubmit}
+                    >ENTER</button>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
