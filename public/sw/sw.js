12a574d3ea685a30a31dc601d5ff1a48219666b1
R0lton <108031948+R0lton@users.noreply.github.com>
2024-08-16 22:41:30 +0300

    ddd

--- public/sw/sw.js
+++ public/sw/sw.js
@@ -1,1 +1,24 @@
-console.log('AAAAAAAAAAAA');
+const CACHE = 'cache-only-v1';
+
+// При установке воркера мы должны закешировать часть данных (статику).
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => {
+            return cache.addAll([
+                '/img/background'
+            ]);
+        })
+    );
+});
+
+// При запросе на сервер (событие fetch), используем только данные из кэша.
+self.addEventListener('fetch', (event) =>
+    event.respondWith(fromCache(event.request))
+);
+
+function fromCache(request) {
+    return caches.open(CACHE).then((cache) =>
+      cache.match(request)
+          .then((matching) => matching || Promise.reject('no-match'))
+    );
+}
